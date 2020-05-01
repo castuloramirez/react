51c971417c04382af8c92d6e5332158ea26da09b
castulo <castuloramirez@gmail.com>
2020-05-02 00:02:43 +0200

    Improving the comments on the grid

--- src/index.js
+++ src/index.js
@@ -2,7 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
- return <div>Hi There!</div>
+    return (
+        <div className="ui container comments">
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img alt="avatar"/>
+                </a>
+                <div className="content">
+                    <a href="/" className="author">
+                        Sam
+                    </a>
+                </div>
+                <div className="metadate">
+                    <span className="date">Today at 23:55</span>
+                </div>
+                <div className="text">
+                    Really good course!
+                </div>
+            </div>
+        </div>
+
+    );
 };
 
-ReactDOM.render(<App></App>,document.querySelector('#root'))
+ReactDOM.render(<App></App>, document.querySelector('#root'))
